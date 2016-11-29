a56a3eb0bf710d2726c193187ba8e36891f56085
Items0 <nieznany012@wp.pl>
2016-11-29 23:40:26 +0100

    Przerwy nie nakrywaja sie

--- Generator/Generator/Generator.cpp
+++ Generator/Generator/Generator.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <conio.h>
+#include <vector>
 /*  kolejnosc argumentow + opis
 1 numer_instancji
 2 liczba_zadan
@@ -18,6 +19,36 @@
 */
 
 using namespace std;
+
+vector < vector <int> > tab(3); //tab[0] nieuzywany
+
+bool porownaj(int czas_startu, int czas_trwania, int nr)
+{
+	bool wartosc = false;
+	if (czas_startu + czas_trwania >= tab[nr].size())
+	{
+		wartosc = true;
+	}
+	else
+	{
+		for (int i = czas_startu; i <= czas_startu + czas_trwania; i++)
+		{
+			if (tab[nr][i])
+			{
+				wartosc = true;
+				break;
+			}
+		}
+		if (wartosc == false)
+		{
+			for (int i = czas_startu; i <= czas_startu + czas_trwania; i++)
+			{
+				tab[nr][i] = 1;
+			}
+		}
+	}
+	return wartosc;
+}
 
 int main()
 {
@@ -33,11 +64,14 @@ int main()
 	int liczba_przerw;
 	int min_czas_przerwy;
 	int max_czas_przerwy;
-	//cout << numer_instancji << endl << liczba_operacji << endl << min_czas_operacji << endl << max_czas_operacji << endl << wspolcznynnik_przerw << endl << liczba_przerw;
+	//cout << numer_instancji << endl << liczba_zadan << endl << min_czas_operacji << endl << max_czas_operacji << endl << wspolcznynnik_przerw << endl << liczba_przerw;
 	int czas_operacji1;
 	int czas_operacji2;
 	int czas_przerwy;
 	int czas_startu_przerwy;
+	int suma_czasow_m1 = 0; // do wyznaczania odpowiednich przerw
+	int suma_czasow_m2 = 0;
+	int nr_maszyny; // jako arg funkcji sprawdzajacej
 	string nazwa;
 	while (uchwyt >> numer_instancji >> liczba_zadan >> min_czas_operacji >> max_czas_operacji >> wspolcznynnik_przerw >> min_czas_przerwy >> max_czas_przerwy)
 	{
@@ -56,24 +90,41 @@ int main()
 			if (rand() % 2)
 			{
 				wyn << "1 2" << endl;
+				suma_czasow_m1 += czas_operacji1;
+				suma_czasow_m2 += czas_operacji2;
 			}
 			else
 			{
 				wyn << "2 1" << endl;
+				suma_czasow_m1 += czas_operacji2;
+				suma_czasow_m2 += czas_operacji1;
 			}
 		}
+		tab[1].assign(suma_czasow_m1, 0);
+		tab[2].assign(suma_czasow_m2, 0);
 		for (int i = 1; i <= liczba_przerw; i++)
 		{
-			wyn << i << " " << 1 + rand() % 2 << " ";
-			czas_przerwy = min_czas_operacji + rand() % (1 + max_czas_przerwy - min_czas_przerwy);
+			nr_maszyny = 1 + rand() % 2;
+			wyn << i << " " << nr_maszyny << " ";
+			czas_przerwy = min_czas_przerwy + rand() % (1 + max_czas_przerwy - min_czas_przerwy);
 			wyn << czas_przerwy << " ";
-			czas_startu_przerwy = rand() % 100; // zrobic na 2 tablicach, aby wykluczac
+			if (suma_czasow_m1 > RAND_MAX || suma_czasow_m2 > RAND_MAX) { cout << "Uwazaj, Losowanie z przedzialu < RAND_MAX, bledy !" << endl; } // potencjalne bledy, wiem w jakim przedziale jestem
+			do
+			{
+				if (nr_maszyny == 1)
+				{
+					czas_startu_przerwy = rand() % suma_czasow_m1;
+				}
+				else
+				{
+					czas_startu_przerwy = rand() % suma_czasow_m2;
+				}
+			} 
+			while (porownaj(czas_startu_przerwy, czas_przerwy, nr_maszyny));
 			wyn << czas_startu_przerwy << endl;
 		}
 		wyn.close();
-		//cout << "here";
 	}
 	_getch();
 	return 0;
 }
-
